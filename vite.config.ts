119834b685d0dc44f81bbb0b36fc8da045536eda
Ashari Muhammad Hisbulloh <asharimh97@gmail.com>
2024-07-04 21:44:51 +0700

    init: initial commit

---
+++ vite.config.ts
@@ -0,0 +1,57 @@
+/// <reference types="vitest" />
+
+import { defineConfig } from "vite";
+import dts from "vite-plugin-dts";
+import { peerDependencies, name } from "./package.json";
+import react from "@vitejs/plugin-react";
+import path from "path";
+
+export default defineConfig({
+  build: {
+    lib: {
+      entry: path.resolve(__dirname, "./src/index.ts"), // Specifies the entry point for building the library.
+      name, // Sets the name of the generated library.
+      fileName: (format) => `index.${format}.js`, // Generates the output file name based on the format.
+      formats: ["umd", "cjs", "es"], // Specifies the output formats (CommonJS and ES modules).
+    },
+    rollupOptions: {
+      external: [...Object.keys(peerDependencies)], // Defines external dependencies for Rollup bundling.
+      output: {
+        globals: {
+          react: "React",
+          "react-dom": "ReactDOM",
+        },
+      },
+    },
+    sourcemap: true, // Generates source maps for debugging.
+    emptyOutDir: true, // Clears the output directory before building.
+  },
+  plugins: [
+    react(),
+    dts({
+      exclude: [
+        "**/*.test.*",
+        "**/*.stories.*",
+        "vite.*.ts",
+        "vitest.*.ts",
+        "test.setup.ts",
+      ],
+    }),
+  ], // Uses the 'vite-plugin-dts' plugin for generating TypeScript declaration files (d.ts).
+  base: "./",
+  resolve: {
+    alias: {
+      "@/src": path.resolve(__dirname, "/src"),
+      "@/commons": path.resolve(__dirname, "/src/commons"),
+      "@/configs": path.resolve(__dirname, "/src/components/configs"),
+      "@/lib": path.resolve(__dirname, "/src/components/lib"),
+      "@/utils": path.resolve(__dirname, "/src/utils"),
+    },
+  },
+  test: {
+    globals: true,
+    setupFiles: "test.setup.ts",
+    css: true,
+    environment: "jsdom",
+  },
+});
