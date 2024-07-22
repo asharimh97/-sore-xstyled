968e07c3afd1282da458cd7df32ed991810a434a
Ashari Muhammad Hisbulloh <asharimh97@gmail.com>
2024-07-22 22:34:41 +0700

    feat: index components

--- src/commons/index.ts
+++ src/commons/index.ts
@@ -1,6 +1,8 @@
+export * from "./alert";
 export { default as Base } from "./base";
 export * from "./box";
 export * from "./button";
 export * from "./divider";
+export * from "./image";
 export * from "./tag";
 export * from "./text";
